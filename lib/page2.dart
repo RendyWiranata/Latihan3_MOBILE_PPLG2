88e176939fb18644d3d5ecc107941ad77b190ce4
RendyWiranata <marhasrendy@gmail.com>
2024-09-10 12:29:19 +0700

    cacatan perubahan

--- lib/page2.dart
+++ lib/page2.dart
@@ -1,3 +1,29 @@
+// import 'package:flutter/material.dart';
+
+// class page2 extends StatelessWidget {
+//   const page2({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Row(children: [ Container(
+//           height:100,
+//           width: 100,
+//           color: Colors.blueAccent,
+//         ),
+//         Row(children: 
+//         [Column(children: 
+//         [Container(
+          
+//         )],)],)
+//         ]
+       
+        
+//       )
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 class page2 extends StatelessWidget {
@@ -6,9 +32,35 @@ class page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('ini halaman 2')
-      )
+      body: Row(children: [
+        Column(
+          children: [
+            Container(height:100,
+            width: 100,
+           color: Colors.red,
+         ),
+         Container(
+          width: 100,
+          height: 100,
+          color: Colors.blue,
+         )
+          ],
+        ),
+        Column(
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.green,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.amber,
+            )
+          ],
+        )
+      ],)
     );
   }
 }
